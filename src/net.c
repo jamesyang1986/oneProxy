01c8c73eaa9d08ce3567a1c7ea108a9e039f1a43
jamesyang <jamesyang@gmail.com>
2024-11-15 18:10:57 +0800

    add net generic method impl

--- src/net.c
+++ src/net.c
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <netdb.h>
 
 #include "net.h"
@@ -50,5 +51,105 @@ int anetSetBlock(int fd, int non_block) {
     }
     return 0;
 }
+
+static void anetSetError(char *err, const char *fmt, ...)
+{
+    va_list ap;
+
+    if (!err) return;
+    va_start(ap, fmt);
+    vsnprintf(err, ANET_ERR_LEN, fmt, ap);
+    va_end(ap);
+}
+
+
+int anetKeepAlive(char *err, int fd, int interval) {
+    int val = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &val, sizeof(val)) == -1)
+    {
+        anetSetError(err, "setsockopt SO_KEEPALIVE: %s", strerror(errno));
+        return -1;
+    }
+    return 1;
+}
+
+static int anetSetTcpNoDelay(char *err, int fd, int val)
+{
+    if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val)) == -1)
+    {
+        anetSetError(err, "setsockopt TCP_NODELAY: %s", strerror(errno));
+        return -1;
+    }
+    return 1;
+}
+
+int anetEnableTcpNoDelay(char *err, int fd)
+{
+    return anetSetTcpNoDelay(err, fd, 1);
+}
+
+int anetSetSendBuffer(char *err, int fd, int buffsize)
+{
+    if (setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &buffsize, sizeof(buffsize)) == -1)
+    {
+        anetSetError(err, "setsockopt SO_SNDBUF: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
+int anetSetRecvBuffer(char *err, int fd, int buffsize) {
+    if (setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &buffsize, sizeof(buffsize)) == -1)
+    {
+        anetSetError(err, "setsockopt SO_RCVBUF: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
+int anetTcpKeepAlive(char *err, int fd)
+{
+    int yes = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(yes)) == -1) {
+        anetSetError(err, "setsockopt SO_KEEPALIVE: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
+int anetSendTimeout(char *err, int fd, long long ms) {
+    struct timeval tv;
+
+    tv.tv_sec = ms/1000;
+    tv.tv_usec = (ms%1000)*1000;
+    if (setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) == -1) {
+        anetSetError(err, "setsockopt SO_SNDTIMEO: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
+int anetReceiveTimeout(char *err, int fd, long long ms) {
+    struct timeval tv;
+
+    tv.tv_sec = ms/1000;
+    tv.tv_usec = (ms%1000)*1000;
+    if (setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) == -1) {
+        anetSetError(err, "set coonnect sockopt SO_SNDTIMEO: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
+
+
+int anetDisableTcpNoDelay(char *err, int fd)
+{
+    return anetSetTcpNoDelay(err, fd, 0);
+}
+
+
+
+
 
 
--- src/net.h
+++ src/net.h
@@ -1,7 +1,18 @@
 #ifndef NET_H
 #define NET_H
 
+#define ANET_ERR_LEN 256
+#define ANET_ERR -1
+#define ANET_OK -1
+
 int socket_bind(const char *ip, int port);
 int anetSetBlock(int fd, int non_block);
+int anetSendTimeout(char *err, int fd, long long ms);
+int anetReceiveTimeout(char *err, int fd, long long ms);
+int anetSetRecvBuffer(char *err, int fd, int buffsize);
+int anetSetSendBuffer(char *err, int fd, int buffsize);
+int anetEnableTcpNoDelay(char *err, int fd);
+int anetTcpKeepAlive(char *err, int fd);
+
 
 #endif
